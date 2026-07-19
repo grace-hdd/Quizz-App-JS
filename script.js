f790494040ec4ec56544ee15c66480247bb6520b
grace haddad <gracehadda@gmail.com>
2026-07-20 00:45:05 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const questions = [
+	{
+		question: "Which is largest animal in the world?",
+		answers: [
+			{ text: "Shark", correct: false},
+			{ text: "Blue whale", correct: true},
+			{ text: "Elephant", correct: false},
+			{ text: "Girrafe", correct: false},
+		]
+	},
+	{
+		question: "Which is the smallest country in the world?",
+		answers: [
+			{ text: "Vatican City", correct: true},
+			{ text: "Bhutan", correct: false},
+			{ text: "Nepal", correct: false},
+			{ text: "Shri Lanka", correct: false},
+		]
+	},
+	{
+		question: "Which is largest desert in the world?",
+		answers: [
+			{ text: "Kalahari", correct: false},
+			{ text: "Gobi", correct: false},
+			{ text: "Sahara", correct: false},
+			{ text: "Antarctica", correct: true},
+		]
+	},
+	{
+		question: "Which is the smallest continent in the world?",
+		answers: [
+			{ text: "Asia", correct: false},
+			{ text: "Australia", correct: true},
+			{ text: "Arctic", correct: false},
+			{ text: "Africa", correct: false},
+		]
+	}
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+	currentQuestionIndex = 0;
+	score = 0;
+	nextButton.innerHTML = "Next";
+	showQuestion();
+}
+
+
